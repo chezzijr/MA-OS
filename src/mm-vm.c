c82897c3941ccf144b350ea45e640b0c94515c54
chezzijr <nhuthuan0501@gmail.com>
2023-12-07 17:55:01 +0700

    fix function accordingly

--- src/mm-vm.c
+++ src/mm-vm.c
@@ -14,17 +14,17 @@
  *@rg_elmt: new region
  *
  */
-int enlist_vm_freerg_list(struct mm_struct *mm, struct vm_rg_struct rg_elmt) {
+int enlist_vm_freerg_list(struct mm_struct *mm, struct vm_rg_struct* rg_elmt) {
 	struct vm_rg_struct *rg_node = mm->mmap->vm_freerg_list;
 
-	if (rg_elmt.rg_start >= rg_elmt.rg_end)
+	if (rg_elmt->rg_start >= rg_elmt->rg_end)
 		return -1;
 
 	if (rg_node != NULL)
-		rg_elmt.rg_next = rg_node;
+		rg_elmt->rg_next = rg_node;
 
 	/* Enlist the new region */
-	mm->mmap->vm_freerg_list = &rg_elmt;
+	mm->mmap->vm_freerg_list = rg_elmt;
 
 	return 0;
 }
@@ -118,12 +118,20 @@ int __alloc(struct pcb_t *caller, int vmaid, int rgid, int size,
  *
  */
 int __free(struct pcb_t *caller, int vmaid, int rgid) {
-	struct vm_rg_struct rgnode;
+	struct vm_rg_struct* rgnode;
 
 	if (rgid < 0 || rgid > PAGING_MAX_SYMTBL_SZ)
 		return -1;
 
 	/* TODO: Manage the collect freed region to freerg_list */
+    rgnode = malloc(sizeof(struct vm_rg_struct));
+    rgnode->rg_start = caller->mm->symrgtbl[rgid].rg_start;
+    rgnode->rg_end = caller->mm->symrgtbl[rgid].rg_end;
+
+    // invalidate the region in symrgtbl
+    caller->mm->symrgtbl[rgid].rg_start = 0;
+    caller->mm->symrgtbl[rgid].rg_end = 0;
+    caller->mm->symrgtbl[rgid].rg_next = NULL;
 
 	/*enlist the obsoleted memory region */
 	enlist_vm_freerg_list(caller->mm, rgnode);
