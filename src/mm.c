467c9535584d9916c4633ccb37b92e7500fc14f1
chezzijr <nhuthuan0501@gmail.com>
2023-12-10 16:28:06 +0700

    ignore allocated frame

--- src/mm.c
+++ src/mm.c
@@ -86,7 +86,7 @@ int vmap_page_range(
 	struct vm_rg_struct *ret_rg)	// return mapped region, the real mapped fp
 {									// no guarantee all given pages are mapped
 	// uint32_t * pte = malloc(sizeof(uint32_t));
-	struct framephy_struct *fpit = malloc(sizeof(struct framephy_struct));
+	// struct framephy_struct *fpit = malloc(sizeof(struct framephy_struct));
 	// int  fpn;
 	int pgit = 0;
 	int pgn = PAGING_PGN(addr);
@@ -94,12 +94,13 @@ int vmap_page_range(
 	ret_rg->rg_end = ret_rg->rg_start =
 		addr; // at least the very first space is usable
 
-	fpit->fp_next = frames;
+	// fpit->fp_next = frames;
 
 	/* TODO map range of frame to address space
 	 *      [addr to addr + pgnum*PAGING_PAGESZ
 	 *      in page table caller->mm->pgd[]
 	 */
+    struct framephy_struct *fpit = frames;
     int ret_val = 0;
     for (pgit = 0; pgit < pgnum; pgit++) {
         if (fpit == NULL) {
